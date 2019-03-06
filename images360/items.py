a34b7c94f916541d9e0cc94719da599f81e14d0d
hongshengzheng <37135497+hongshengzheng@users.noreply.github.com>
2019-03-06 12:44:26 +0800

    Add files via upload

---
+++ images360/items.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy import Item,Field
+
+
+class ImageItem(Item):
+    collection =table ='images'
+    id =Field()
+    url =Field()
+    title =Field()
+    thumb =Field()
+
+    # define the fields for your item here like:
+    # name = scrapy.Field()
